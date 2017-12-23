26550a36837bd258bc880cf91fb4e84d4e27a0c6
Antonio Perez Caballero <antonio.gialnet@gmail.com>
2017-12-23 18:16:45 +0000

    First commit

---
+++ src/main/java/es/gialnet/prodacon/regantes/RegantesApplication.java
@@ -0,0 +1,12 @@
+package es.gialnet.prodacon.regantes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegantesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegantesApplication.class, args);
+	}
+}
